861479136c598535af0d9de793556874ed035789
Bo (Aaron) Han <obnah.chn@gmail.com>
2020-05-17 18:22:08 +0800

    add start.sh

---
+++ start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export RELX_REPLACE_OS_VARS=true
+export MARKVPASS=default
+
+
+if [[ $MARKVPASS == default ]] ; then
+	echo "You are using default password, type Enter to continue with default password or the password you'd like. CTRL+C to break."
+	read -s pswd
+	case $pswd in
+		"" )
+			echo "using default password!"
+			;;
+		* )
+			export MARKVPASS=$pswd
+			;;
+	esac
+fi
+
+while true
+do
+	~aaronhan/share/markV_release.run
+	[[ $? -ne 233 ]] && break
+done
+
